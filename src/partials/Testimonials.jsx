ca4d18f23e1c274e09e67978660f1ad657aa497d
akirademoss <akirademoss@gmail.com>
2023-04-06 14:07:38 -0700

    website updates

--- src/partials/Testimonials.jsx
+++ src/partials/Testimonials.jsx
@@ -8,18 +8,18 @@ function Testimonials() {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20 border-t border-gray-800">
+        <div className="py-12 md:py-10 border-t border-gray-800">
 
-          {/* Section header */}
+          {/* Section header 
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">Don't take our word for it</h2>
             <p className="text-xl text-gray-400">Read more about why our clients love us and keep coming back for our services.</p>
-          </div>
+          </div>*/}
 
-          {/* Testimonials */}
+          {/* Testimonials 
           <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
 
-            {/* 1st testimonial */}
+            {/* 1st testimonial 
             <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up">
               <div>
                 <div className="relative inline-flex flex-col mb-4">
@@ -35,7 +35,7 @@ function Testimonials() {
                 </div>
               </div>
 
-              {/* 2nd testimonial */}
+              {/* 2nd testimonial 
               <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="200">
                 <div>
                   <div className="relative inline-flex flex-col mb-4">
@@ -51,7 +51,7 @@ function Testimonials() {
                   </div>
                 </div>
 
-                {/* 3rd testimonial */}
+                {/* 3rd testimonial 
                 <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
                   <div>
                     <div className="relative inline-flex flex-col mb-4">
@@ -67,7 +67,7 @@ function Testimonials() {
                     </div>
                   </div>
 
-                </div>
+                </div>*/}
 
               </div>
             </div>
