a1cf3d8b4a98947fa5a827751936929fb0a3d8d5
Akira DeMoss <akirademoss@gmail.com>
2024-10-20 16:16:31 -0700

    starting updating pricing info

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -5,41 +5,276 @@ import Header from '../partials/Header';
 import PageIllustration from '../partials/PageIllustration';
 import Banner from '../partials/Banner';
 
-function SignIn() {
+import {
+  Typography,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+} from "@material-tailwind/react";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
+import { MinusCircleIcon } from "@heroicons/react/24/outline";
+
+function PricingCard({ title, desc, price, options }) {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
 
-      {/*  Site header */}
-      <Header />
+    <Card variant="gradient" color="white">
+      <CardHeader
+        floated={false}
+        shadow={false}
+        color="transparent"
+        className="!m-0 p-6"
+      >
+        <Typography
+          variant="h6"
+          color="blue-gray"
+          className="capitalize font-bold mb-1"
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="small"
+          className="font-normal !text-gray-500"
+        >
+          {desc}
+        </Typography>
+        <Typography
+          variant="h3"
+          color="blue-gray"
+          className="!mt-4 flex gap-1 !text-4xl"
+        >
+          {price[0]}
+          {price[1]}
+          <Typography
+            as="span"
+            color="blue-gray"
+            className="-translate-y-0.5 self-end opacity-70 text-lg font-bold"
+          >
+            /{price[2]}
+          </Typography>
+        </Typography>
+      </CardHeader>
+      <CardBody className="pt-0">
+        <ul className="flex flex-col gap-3 mb-6">
+          {options.map((option, key) => (
+            <li
+              key={key}
+              className="flex items-center gap-3 text-gray-700"
+            >
+              {option.icon}
+              <Typography
+                variant="small"
+                className="font-normal text-inherit"
+              >
+                {option.info}
+              </Typography>
+            </li>
+          ))}
+        </ul>
+        <Button className="text-white" fullWidth varient="gradient" color="gray">
+          get started
+        </Button>
+      </CardBody>
+    </Card>
 
-      {/*  Page content */}
-      <main className="grow">
+  );
+}
 
-        {/*  Page illustration */}
-        <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
-          <PageIllustration />
+export function PricingSection11() {
+  const cards = [
+    {
+      title: "Monthly",
+      desc: "Every 4 weeks",
+      price: ["$", "85.50", "hr"],
+      options: [
+        {
+          icon: (
+            <CheckCircleIcon className="h-5 w-5 text-blue-gray-900" />
+          ),
+          info: "Complete documentation",
+        },
+        {
+          icon: (
+            <CheckCircleIcon className="h-5 w-5 text-blue-gray-900" />
+          ),
+          info: "Working materials in Sketch",
+        },
+        {
+          icon: (
+            <MinusCircleIcon
+              strokeWidth={2.5}
+              className="h-5 w-5 text-blue-gray-900"
+            />
+          ),
+          info: "Integration help",
+        },
+        {
+          icon: (
+            <MinusCircleIcon
+              strokeWidth={2.5}
+              className="h-5 w-5 text-blue-gray-900"
+            />
+          ),
+          info: "40GB Cloud storage",
+        },
+        {
+          icon: (
+            <MinusCircleIcon
+              strokeWidth={2.5}
+              className="h-5 w-5 text-blue-gray-900"
+            />
+          ),
+          info: "Support team full assist",
+        },
+      ],
+    },
+    {
+      title: "Bi-weekly",
+      desc: "Every 2 weeks",
+      price: ["$", "80.75", "hr"],
+      options: [
+        {
+          icon: (
+            <CheckCircleIcon className="h-5 w-5 text-blue-gray-900" />
+          ),
+          info: "Complete documentation",
+        },
+        {
+          icon: (
+            <CheckCircleIcon className="h-5 w-5 text-blue-gray-900" />
+          ),
+          info: "Working materials in Sketch",
+        },
+        {
+          icon: (
+            <CheckCircleIcon
+              strokeWidth={2.5}
+              className="h-5 w-5 text-blue-gray-900"
+            />
+          ),
+          info: "Integration help",
+        },
+        {
+          icon: (
+            <CheckCircleIcon
+              strokeWidth={2.5}
+              className="h-5 w-5 text-blue-gray-900"
+            />
+          ),
+          info: "40GB Cloud storage",
+        },
+        {
+          icon: (
+            <MinusCircleIcon
+              strokeWidth={2.5}
+              className="h-5 w-5 text-blue-gray-900"
+            />
+          ),
+          info: "Support team full assist",
+        },
+      ],
+    },
+    {
+      title: "One-Time",
+      desc: "One-Time deep cleaning",
+      price: ["$", "95", "year"],
+      options: [
+        {
+          icon: (
+            <CheckCircleIcon className="h-5 w-5 text-blue-gray-900" />
+          ),
+          info: "Complete documentation",
+        },
+        {
+          icon: (
+            <CheckCircleIcon className="h-5 w-5 text-blue-gray-900" />
+          ),
+          info: "Working materials in Sketch",
+        },
+        {
+          icon: (
+            <CheckCircleIcon
+              strokeWidth={2.5}
+              className="h-5 w-5 text-blue-gray-900"
+            />
+          ),
+          info: "Integration help",
+        },
+        {
+          icon: (
+            <CheckCircleIcon
+              strokeWidth={2.5}
+              className="h-5 w-5 text-blue-gray-900"
+            />
+          ),
+          info: "40GB Cloud storage",
+        },
+        {
+          icon: (
+            <CheckCircleIcon
+              strokeWidth={2.5}
+              className="h-5 w-5 text-blue-gray-900"
+            />
+          ),
+          info: "Support team full assist",
+        },
+      ],
+    },
+  ];
+
+  return (
+    
+    <div className="flex flex-col min-h-screen overflow-hidden ">
+
+    {/*  Site header */}
+    <Header />
+    <div className="flex justify-center align-center">
+    <section className="py-24 px-8">
+      <div className="container mx-auto">
+        <Typography
+          color="blue-gray"
+          className="mb-4 font-bold text-lg"
+        >
+          Pricing Plans
+        </Typography>
+        <Typography
+          variant="h1"
+          color="blue-gray"
+          className="mb-4 !leading-snug lg:!text-4xl !text-2xl max-w-2xl"
+        >
+          Invest in a plan that&apos;s as ambitious as your corporate goals.
+        </Typography>
+        <Typography
+          variant="lead"
+          className="mb-10 font-normal !text-gray-500 max-w-xl"
+        >
+          Compare the benefits and features of each plan below to find the ideal
+          match for your business&apos;s budget and ambitions.
+        </Typography>
+        <div className="grid gap-x-10 gap-y-8 md:grid-cols-2 lg:grid-cols-3 max-w-5xl">
+          {cards.map(({ title, desc, options, price }, key) => (
+            <PricingCard
+              key={key}
+              title={title}
+              desc={desc}
+              price={price}
+              options={options}
+            />
+          ))}
         </div>
-
-        <section className="relative">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6">
-            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
-              {/* Page header */}
-              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h1 className="h1">Pricing Info Coming Soon.</h1>
-              </div>
-
-              
-            </div>
-          </div>
-        </section>
-
-      </main>
-
-      <Banner />
-
+        <Typography
+          variant="small"
+          className="mt-10 font-normal !text-gray-500"
+        >
+          You have Free Unlimited Updates and Premium Support on each package.
+          You also have 30 days to request a refund.
+        </Typography>
+      </div>
+    </section>
+    </div>
     </div>
   );
 }
 
-export default SignIn;
+export default PricingSection11;
+
