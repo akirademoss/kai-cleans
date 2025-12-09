4e0a01f3f7a96f2686c6c79d9028f86f43ae327d
Akira DeMoss <akirademoss@gmail.com>
2025-12-09 09:54:11 -0800

    updated pricing banner - no more automatic 10% off

--- src/partials/Banner8.jsx
+++ src/partials/Banner8.jsx
@@ -7,14 +7,14 @@ const Banner8 = () => {
         <div className="mx-auto container py-12 px-6 xl:px-0 flex justify-center items-center flex-col" data-aos="fade-up" style={{paddingBottom: "0px", width: '100%'}}>
             <div className="flex justify-between bg-gray-50 items-stretch flex-row relative" style={{width: '100%'}}>
                 <div className="flex items-center bg-gray-600 justify-center" >
-                    <p className="transform flex flex-shrink-0 -rotate-90 text-2xl font-semibold tracking-wide leading-normal text-white">10% OFF</p>
+       
                 </div>
                 <div className="flex justify-center items-start flex-col xl:w-2/5 md:w-5/12 xl:px-7 px-6 md:px-0 md:py-0 py-5">
                     <div>
-                        <p className="text-3xl xl:text-4xl font-semibold leading-9 text-gray-800">Act before it’s too late!</p>
+                        <p className="text-3xl xl:text-4xl font-semibold leading-9 text-gray-800">Reclaim your space</p>
                     </div>
                     <div className="xl:mt-4 mt-2">
-                        <p className="text-base xl:text-xl leading-7 text-gray-600 pr-4">Take an additional 10% off your first clean!</p>
+                        <p className="text-base xl:text-xl leading-7 text-gray-600 pr-4">Thoughtful, detail-driven cleaning that supports mental clarity and well-being.</p>
                     </div>
                 </div>
                 <div className="hidden md:block h-44 md:h-60 xl:h-72">
